5872ed2ec94e4399930f221d48976d2d960c8d1a
MatthiasBradshaw <matthias.bradshaw@snhu.edu>
2026-05-20 09:47:13 -0700

    fixed division by zero

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
@@ -1,5 +1,3 @@
-/************************************
- */
 package org.eclipse.example.calc.internal.operations;
 
 import org.eclipse.example.calc.BinaryOperation;
@@ -13,8 +11,12 @@ public class Divide extends AbstractOperation implements BinaryOperation {
 	// floating point division
 	@Override
 	public float perform(float arg1, float arg2) {
-		return arg1 / arg2;
+	    if (arg2 != 0) {
+	        return arg1 / arg2;
+	    }
+	    return Float.NaN;
 	}
+		
 
 	
 	@Override
