363ca9f37e6387c8525f021f396c8a2d0b666026
MatthiasBradshaw <matthias.bradshaw@snhu.edu>
2026-05-20 11:13:01 -0700

    add multiply functionality

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -18,6 +18,7 @@ import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
 import org.eclipse.example.calc.internal.operations.Divide; // import divide class
+import org.eclipse.example.calc.internal.operations.Multiply; // import multiply class
 
 public class Calculator {
 
@@ -42,6 +43,8 @@ public class Calculator {
 		new Plus();
 		new Square();
 		new Divide();  // added divide functionality
+		new Multiply(); // added multiply functionality
+		
 	}
 
 	// fixed: textProvider value may be null or empty String
