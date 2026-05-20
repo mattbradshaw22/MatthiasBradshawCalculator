34d9947861148742448495ef283e920583f8f1ee
MatthiasBradshaw <matthias.bradshaw@snhu.edu>
2026-05-20 15:24:25 -0700

    intial modulo implementation

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Modulo.java
@@ -0,0 +1,26 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+/**
+ * Binary modulo operation
+ */
+
+public class Modulo extends AbstractOperation implements BinaryOperation {
+
+	// floating point modulo
+	@Override
+	public float perform(float arg1, float arg2) {
+	    if (arg2 == 0) {
+	        return Float.NaN;
+	    }
+	    return arg1 % arg2;
+	}
+		
+	
+	@Override
+	public String getName() {
+		return "%";
+	}
+
+}
