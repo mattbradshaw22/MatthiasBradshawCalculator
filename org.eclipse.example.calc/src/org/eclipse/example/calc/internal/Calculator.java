5c272fe47a3900f3b52402b044d32224ed31001c
MatthiasBradshaw <matthias.bradshaw@snhu.edu>
2026-05-20 15:35:21 -0700

    add modulo functionality

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -20,7 +20,7 @@ import org.eclipse.example.calc.internal.operations.Square;
 import org.eclipse.example.calc.internal.operations.Divide; // import divide class
 import org.eclipse.example.calc.internal.operations.Multiply; // import multiply class
 import org.eclipse.example.calc.internal.operations.Power; // import power class
-
+import org.eclipse.example.calc.internal.operations.Modulo; // import modulo class
 
 public class Calculator {
 
@@ -47,6 +47,7 @@ public class Calculator {
 		new Divide();  // added divide functionality
 		new Multiply(); // added multiply functionality
 		new Power(); // added power functionality
+		new Modulo(); //added modulo functionality
 
 	}
 
