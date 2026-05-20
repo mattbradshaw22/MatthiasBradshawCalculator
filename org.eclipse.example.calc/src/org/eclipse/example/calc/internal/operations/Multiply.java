473b185212610e2d5a70601292907f8c44cfdb23
MatthiasBradshaw <matthias.bradshaw@snhu.edu>
2026-05-20 11:13:01 -0700

    add multiply functionality

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
@@ -1,5 +1,24 @@
 package org.eclipse.example.calc.internal.operations;
 
-public class Multiply {
+import org.eclipse.example.calc.BinaryOperation;
 
+/**
+ * Binary multiply operation
+ */
+
+public class Multiply extends AbstractOperation implements BinaryOperation {
+	
+	@Override
+	public float perform(float arg1, float arg2) {
+		return arg1 * arg2;
+	}
+
+	@Override
+	public String getName() {
+		return "*";
+	}
+	
 }
+
+// aditional change test
+
